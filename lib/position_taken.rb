e0a5ea7de7649aea243c6a899939ce44d1a551b2
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-01 16:13:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,12 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, index_number)
-  !array[index_number] == " " || !array[index_number] == "" || !array[index_number] == nil
+  if array[index_number] == " "
+    false
+  elsif array[index_number] == ""
+    false
+  elsif array[index_number] == nil
+    false
+  else
+    true
 end
