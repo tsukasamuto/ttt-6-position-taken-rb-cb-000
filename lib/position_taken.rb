7cff12c73c6ed1195c233f507820222657d97aac
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-01 16:14:45 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, index_number)
+  if array[index_number] == " "
+    false
+  elsif array[index_number] == ""
+    false
+  elsif array[index_number] == nil
+    false
+  else
+    true
+  end
+end
